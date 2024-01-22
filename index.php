53c62b316f7834b6443b2dfba50761b40e19914f
fathursyh <fathur.syah1@gmail.com>
2024-01-22 22:54:45 +0700

    update practice.php

--- index.php
+++ index.php
@@ -24,35 +24,7 @@
         <div class="paper">
             <p>malam tahun baru tersisa <?php echo "$days" ?> hari lagi</p>
             <?php
-            // VARIABLES AND STRING MANIPULATIONS
-                echo "<p>fathur itu ", $ganteng,"</p>";
-                echo "<p>panjang tongkat itu adalah $panjang cm</p>";
-                echo "<p>", str_repeat($jurusan, 2), "</p>";
-                echo "<p>Fathur ", str_replace('banget', 'sekali', $ganteng), "<br></p";
-                echo "<p>", substr($paragraf, 0, 40), "</p>";
-            // CONTROL
-                if (date('l') == 'Saturday') {
-                    echo "<p><strong>kami tutup</strong></p>";
-                } else {
-                    echo "<p>kami buka dari jam 7</p>";
-                }
-                echo "<p>WHILE LOOP</p>";
-                while ($panjang < 20) {
-                        echo $panjang, "<br>";
-                        $panjang++;
-                }
-                echo "<p>FOR LOOP</p>";
-                echo "<hr>";
-                for ($i = 0; $i < 5; $i++) {
-                    echo $i, "<br>";
-                }
-                
-                echo $cantik, "<br>";
-                echo $dia;
-                nl();
-                echo gettype($suhu);
-                var_dump($ujan);
-                print_r($hewan);
+            include_once 'practice.php';
             ?>
         </div>
     </div>
